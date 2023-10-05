046b10f2cf4ce3afed90794c96dfdd567bd6b918
Milana46 <139504932+Milana46@users.noreply.github.com>
2023-10-06 02:39:06 +0300

    Delete src directory

--- src/Fridge.java
+++
@@ -1,7 +0,0 @@
-public class Fridge extends Pribor {
-    public Fridge(Integer P,String name) {
-        super(P,name);
-    }
-
-
-}
--- src/Iron.java
+++
@@ -1,7 +0,0 @@
-public class Iron extends Pribor{
-
-    public Iron(Integer P,String name) {
-        super(P, name);
-    }
-
-}
